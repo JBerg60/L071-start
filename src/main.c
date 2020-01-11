3c3cc09fcb9d20db7f9bed7df39956a26f49aba9
unknown <johnnyvandeberg@gmai.com>
2020-01-11 10:31:35 +0100

    enable 32Mhz clock, using HSE with xtal 8.0 MHz

--- src/main.c
+++ src/main.c
@@ -42,10 +42,14 @@ struct PC13 : public Pindef<13, 2>
     static constexpr uint32_t base{GPIOC_BASE};
 };
 
+void SystemInit();
+
 int main(void)
 {
     RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
     RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+
+    SystemInit();
 
     Os os;
 
@@ -86,4 +90,53 @@ int main(void)
 
         bt.send("Hello!\r\n");
     }
+}
+
+// Change system clock to 32 MHz using 8 MHz crystal
+void SystemInit()
+{
+    // Because the debugger switches PLL on, we may
+    // need to switch back to the HSI oscillator without PLL
+
+    // Switch to HSI oscillator
+    MODIFY_REG(RCC->CFGR, RCC_CFGR_SW, RCC_CFGR_SW_HSI);
+
+    // Wait until the switch is done
+    //while ((RCC->CFGR & RCC_CFGR_SWS_Msk) != RCC_CFGR_SWS_HSI)
+    //{
+    //}
+
+    // Disable the PLL, then we can configure it
+    CLEAR_BIT(RCC->CR, RCC_CR_PLLON);
+
+    // Flash latency 1 wait state
+    SET_BIT(FLASH->ACR, FLASH_ACR_LATENCY);
+
+    // Enable HSE oscillator
+    SET_BIT(RCC->CR, RCC_CR_HSEON);
+
+    // Wait until HSE oscillator is ready
+    while (!READ_BIT(RCC->CR, RCC_CR_HSERDY))
+    {
+    }
+
+    // 32 MHz using the 8 MHz HSE oscillator multiply by 8 divide by 2
+    WRITE_REG(RCC->CFGR, RCC_CFGR_PLLSRC_HSE + RCC_CFGR_PLLMUL8 + RCC_CFGR_PLLDIV2);
+
+    // Enable PLL
+    SET_BIT(RCC->CR, RCC_CR_PLLON);
+
+    // Wait until PLL is ready
+    while (!READ_BIT(RCC->CR, RCC_CR_PLLRDY))
+    {
+    }
+
+    // Select PLL as clock source
+    MODIFY_REG(RCC->CFGR, RCC_CFGR_SW, RCC_CFGR_SW_PLL);
+
+    // Update variable
+    SystemCoreClock = 32000000;
+
+    // Switch the MSI oscillator off
+    CLEAR_BIT(RCC->CR, RCC_CR_MSION);
 }
